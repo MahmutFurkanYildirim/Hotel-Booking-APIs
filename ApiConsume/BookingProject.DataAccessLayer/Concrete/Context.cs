17ff911ee81cfb8297ddcca9c950f40f6616e241
Furkan YILDIRIM <46926199+Furkany99@users.noreply.github.com>
2024-06-27 22:38:46 +0300

    DataAccessLayer

---
+++ ApiConsume/BookingProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,20 @@
+﻿using BookingProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BookingProject.DataAccessLayer.Concrete
+{
+	public class Context:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=DESKTOP-B9IE91I;initial catalog=BookingApi;integrated security=true");
+		}
+
+		public DbSet<Room> Rooms { get; set; }
+		public DbSet<Service> Services { get; set; }
+		public DbSet<Staff> Staffs { get; set; }
+		public DbSet<Subscribe> Subscribes { get; set; }
+		public DbSet<Testimonial> Testimonials { get; set; }
+	}
+}
