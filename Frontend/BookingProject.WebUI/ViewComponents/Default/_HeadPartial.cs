57659f8f9511bb20e0cffe2a6f23fdcfefc650a2
Furkan YILDIRIM <46926199+Furkany99@users.noreply.github.com>
2024-07-01 16:28:05 +0300

    View Components

---
+++ Frontend/BookingProject.WebUI/ViewComponents/Default/_HeadPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingProject.WebUI.ViewComponents.Default
+{
+    public class _HeadPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
