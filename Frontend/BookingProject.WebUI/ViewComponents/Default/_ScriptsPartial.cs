d7fb599b9b0c8da24b96ee5bcc2ed254c9ddc936
Furkan YILDIRIM <46926199+Furkany99@users.noreply.github.com>
2024-07-01 17:07:16 +0300

    Components(Partial)

---
+++ Frontend/BookingProject.WebUI/ViewComponents/Default/_ScriptsPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingProject.WebUI.ViewComponents.Default
+{
+    public class _ScriptsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/BookingProject.WebUI/ViewComponents/Default/_SpinnerPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingProject.WebUI.ViewComponents.Default
+{
+    public class _SpinnerPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
