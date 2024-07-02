a9b919b1288839d73ea744d05dd9f6e66d373c4c
Furkan YILDIRIM <46926199+Furkany99@users.noreply.github.com>
2024-07-02 22:00:06 +0300

    About(Service,Manager,Ef)

---
+++ ApiConsume/BookingProject.BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,40 @@
+﻿using BookingProject.BusinessLayer.Abstract;
+using BookingProject.DataAccessLayer.Abstract;
+using BookingProject.EntityLayer.Concrete;
+
+namespace BookingProject.BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private readonly IAboutDal _aboutDal;
+
+        public AboutManager()
+        {
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutDal.Delete(t);
+        }
+
+        public About TGetById(int id)
+        {
+            return _aboutDal.GetById(id);
+        }
+
+        public List<About> TGetList()
+        {
+            return _aboutDal.GetList();
+        }
+
+        public void TInsert(About t)
+        {
+            _aboutDal.Insert(t);
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+        }
+    }
+}
---
+++ ApiConsume/BookingProject.DataAccessLayer/EntityFramework/EfAboutDal.cs
@@ -0,0 +1,14 @@
+﻿using BookingProject.DataAccessLayer.Abstract;
+using BookingProject.DataAccessLayer.Concrete;
+using BookingProject.DataAccessLayer.Repositories;
+using BookingProject.EntityLayer.Concrete;
+
+namespace BookingProject.DataAccessLayer.EntityFramework
+{
+    public class EfAboutDal : GenericRepository<About>, IAboutDal
+    {
+        public EfAboutDal(Context context) : base(context)
+        {
+        }
+    }
+}
